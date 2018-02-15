4c63a2461d4f9e33b346e67684d3647f52de1c79
d.pasquali <d.pasquali@reply.it>
2018-02-15 19:50:11 +0100

    aaa

--- src/main/scala/it/reply/data/devops/KuduStorage.scala
+++ src/main/scala/it/reply/data/devops/KuduStorage.scala
@@ -1,16 +1,25 @@
 package it.reply.data.devops
 
 import it.reply.data.pasquali.Storage
+import org.apache.spark.sql.Row
 
 case class KuduStorage(storage : Storage) {
 
   def storeRatingsToKudu(filename : String) : Unit = {
 
-    val table = storage.spark.read
+    val spark = storage.spark
+
+    import spark.implicits._
+
+    val table = spark.read
       .format("csv")
       .option("header", "true")
       .option("mode", "DROPMALFORMED")
       .load(filename)
+      .map{ case Row(userID, movieID, rating, time) =>
+        Row(userID.asInstanceOf[Long],
+          movieID.asInstanceOf[Long],
+          rating.asInstanceOf[Double], time)}.toDF("userid", "movieid", "rating", "time")
 
     storage.upsertKuduRows(table, "default.kudu_ratings")
   }
